b69facc05e2a6f6a0b4465e1edd5fbe55713e6a2
paul_sempijja <110895289+plwevuze@users.noreply.github.com>
2022-09-07 23:57:13 -0700

    Delete hello-world.js
    delete

--- hello-world.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world paul");
-console.log("programming is fun");
-//declaring in javascript
-let myname = "PAUL_SEMPIJJA";
-console.log(myname)
-let num1 = 10;
-console.log (num1)
