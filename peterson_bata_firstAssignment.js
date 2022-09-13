39d82256576f9fe962c44676dc3b56f212b829a4
Peterson Batatwenda <mwesigyepeterson38@gmail.com>
2022-09-13 09:18:57 +0300

    FirstAssignment ongoing

---
+++ peterson_bata_firstAssignment.js
@@ -0,0 +1,58 @@
+let you =" are the reason";
+//A variable "you" is assigned a value "are the reason" which is a string.
+var we = " the next generation of programmer";
+//Variable "we" is assigned  a value "the next generation of programmer".
+const us = ' we are upcoming software problem solvers';
+//A constant variable is declared "us" and assigned value 'we are upcoming software problem solvers'.
+let num1 = 5;
+//Value "5" which is a number is assigned to a declared variable 'num1'.
+let num2 = 10;
+//Value "10" is assigned to a declared variable 'num2'.
+let num3 = num2 += num1;
+//The value of the declared variable num3 is assigned the concantination of values for variables num2 and num1 (10+5) which is 15.
+let ans = num1 % num2;
+//The variable declared "ans" is assigned the remainder of the divided values of variables num1 and num2.
+console.log(ans);
+//Displaying the current value of variable declared ans which is 15.
+console.log(num3 *= 2);
+//The value of the declared variable "num3" is multiplied by 2 ,so the value to be displayed will be 30.
+var num4;
+//The value of the declared variable "num4" is undefined.
+console.log(num4);
+//The display is "undefined" since the variable num4 lacks a value.
+console.log(2num6);
+//Since the variable name begins with a number, it returns as an error because variable names should neither begin with a number nor capital letter unless when using camelCase or underscore.
+var ans = 'this is not right';
+//A variable declared "ans" is assigned value 'this is not right'.
+var let ='do you know what you are doing?';
+//Its not advisable to declare a variable with a key-word (var,let,const,class etc), so it returns an error.
+const dad;
+//A constant variable declared "dad" lacks a value , so its undefined.
+const myNum = num1>num2? num3:us;
+//Declare a constant variable "myNum" and assign it the value of num3 if the value of num1 is greater than the value of num2 but if not , the value the variable myNum will be that of variable "us".
+console.log(--num3);
+//Pre-decrement decreases 1 from the value of num3 at first and display result as "29".
+console.log(num3);
+//Display "29" as the value of num3 since 1 was subtracted from the value of num3 which was "30".
+console.log(num3++);
+//Post-increment adds 1 to the value of num3 and display 29 as return at first , then displays "30" as return after.
+console.log(num2 != num3);
+//The value of num2 is not equal to value of num3 so it displays true since value of num2 is not equal to that of num3.
+console.log(num3 !=num2 && num2==num2);
+//The display returns true since its true that num3 is not equal to num2 while num2 is equal to num2 or esle it would be false.
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+//Assign value 20 to num2 and display 'you are now a junior developer' if its true that 100 is greater than or equal to 200 or else nothing will be displayed.
+
+
+
+
+
+
+
+
+
+
