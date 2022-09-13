2e745479da5eac2a63fdecdb251bbf3b2e88c858
josephat <josephat.kayanja@gmail.com>
2022-09-13 08:05:20 +0300

    coursework

---
+++ josephat.js
@@ -0,0 +1,48 @@
+let you =" are the reason";
+//initialization and declaration of string you
+var we = " the next generation of programmer";
+// initialization and declaration of string we
+const us = ' we are upcoming software problem solvers';
+//declaration and initialization of constant us
+let num1 = 5;
+// declaring variable num1 and initializing it to 5
+let num2 = 10;
+// declaring variable num2 and initializing it to 10
+let num3 = num2 += num1;
+// declaring variable num3 and assigning it to num2 plus or equal to num1
+let ans = num1 % num2;
+// declare variable ans and assign it to the modulus of num1 and num2
+console.log(ans);
+// output the value of ans to the web console
+console.log(num3 *= 2);
+//output the result of num3 * 2 to the web console
+var num4;
+// declare the variable num4
+console.log(num4);
+// output the value of num4 to the web console 
+console.log(2num6);
+//this is an error
+var ans = 'this is not right';
+// declare ans and assign it to the string "this is not right"
+var let ='do you know what you are doing?';
+// this is an error
+const dad;
+//declare constant dad
+const myNum = num1>num2? num3:us;
+// if num1 is greater than num2, assign it to myNum else assign us to myNum
+console.log(--num3);
+// output the value of num3 after decreasing it by 1
+console.log(num3);
+//output the value of num3 to the web console
+console.log(num3++);
+//output the num3 before incrementing it by 1
+console.log(num2 != num3);
+//if num2 is not equal to num3, output true else output false
+console.log(num3 !=num2 && num2==num2);
+//if num3 is not equal to num2 and num2 is equal to num2, output true to the web console else output false
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+// this code has no output
