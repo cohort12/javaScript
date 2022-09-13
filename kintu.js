fc8876008bace2982345a37deb8db38fa30e0078
kintu riley davis <deviskintu@gmail.com>
2022-09-13 13:46:27 +0300

    added file

---
+++ kintu.js
@@ -0,0 +1,60 @@
+let you =" are the reason";
+//A key word "let" is used to declare  a variable  "you" which initializes a string value "are the reason".
+var we = " the next generation of programmer";
+//A key word "var" is used to declare a variable "we" which initializes a string value "the next generation of programmer".
+const us = ' we are upcoming software problem solvers';
+//A key word "const" is used to declare a variable "us" which initializes a string value 'we are upcoming software problem solvers' .
+let num1 = 5;
+//Here we declare a variable "num1" with a value of 5.
+let num2 = 10;
+//Hre we declare a variable "num2" with a value of 10.
+let num3 = num2 += num1;
+//Here a variable "num3" is assigned to the sum of two variables "num2" and "num1" having the 15 as the answer and value of "num3".
+let ans = num1 % num2;
+//A variable "ans" is declared hence being assigned to the value got after dividing num1 with num2 which is 5.
+console.log(ans);
+//after terminating the answer is 5
+console.log(num3 *= 2);
+//here we multiply the value assigned to variable num3 by 2 giving the answer of 30.
+var num4;
+//A key word "var" is used to declare a variable "num4" which is not initialized.
+console.log(num4);
+//here the variable "num4" value  will come out as undefined since variable "num4" is not initialized and assigned to any value.
+console.log(2num6);
+//here a "syntaxError" will be printed out having terminated because the variable "num6" its beginning with a number "2" yet variables should not begin numbers.
+var ans = 'this is not right';
+//here we will get a "syntaxError" because the variable "ans" was already declared in line 13.("a variable can not be declared twice")
+var let ='do you know what you are doing?';
+//here both "var" and "let" are keywords used in Javascript so we are going to get a syntax error since we cant use keywords as variables.
+const dad;
+//here the variable "dad" is not declared so on terminating a syntax error will be returned.
+const myNum = num1>num2? num3:us;
+//a keyword "const" declares a variable "myNum" whose initial value is to be got by comparing if(num1 > num2), if its correct then "num3" is assigned to "myNum" else sign it to the value of variable "us".
+console.log(--num3);
+//here we decrement the variable "num3" and our answer will be 29.
+console.log(num3);
+//here variable "num3" will still have the some value of 29 because of the decrement we made in line 33.
+console.log(num3++);
+//here we are going to first get the value of variable "num3" which is 29 then increment it with 1.
+console.log(num2 != num3);
+//here we get a return of TRUE since num2 is not equal to num3.
+console.log(num3 !=num2 && num2==num2);
+//here if the value of variable "num3" is not equal to that of "num2" then "num2" is equal to the value of variable "num2" hence returning TRUE as the answer.
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+
+//if its true 100 is greater than or equal to 200  assign variable "num2" with value of 20 then print the text string "you are now a junior developer".
+
+
+
+
+
+
+
+
+
+
+
