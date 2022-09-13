f75970526af5de4e8263166eda0a6cacac12bfbe
Ainomugisha3 <111879736+Ainomugisha3@users.noreply.github.com>
2022-09-13 15:14:52 +0300

    Add files via upload

---
+++ Isaac/javaScript/b_alinda_1.js
@@ -0,0 +1,98 @@
+let you =" are the reason";
+// declares a variable named 'you' 
+// and initiates it with a string value - " are the reason"
+
+var we = " the next generation of programmer";
+// declares a variable named 'we' 
+// and initiates it with a string value " the next generation of programmer"
+
+const us = ' we are upcoming software problem solvers';
+// declares a constant named 'us' and 
+// initiates it with a string value - ' we are upcoming software problem solvers'
+
+let num1 = 5;
+// declares a varibale named num1 and initiates it with a value of five
+
+let num2 = 10;
+// declares a variable named num2 and initiates it with a value of ten
+
+let num3 = num2 += num1;
+// declares a variable named num3 and
+// initiates it with a calculation | num2 is added to num1 | the result is assigned to num3
+// num3 = 15
+
+let ans = num1 % num2;
+// declares a variable named ans and initiates it with a calculation
+// this calculates the remainder of num1 divided by num2 which is assigned to ans
+// ans = 5
+// (there is an error because the variable has been decalred again below)
+
+console.log(ans);
+// displays the value of the varibale ans in the terminal
+
+console.log(num3 *= 2);
+// displays the value of num3 being multiplied and assigned to itself 
+// displays -> 30
+
+var num4;
+// displays the variable num4 and soes not initiate it with a value
+
+console.log(num4);
+// displays the value of num4 in the terminal (undefined)
+
+console.log(2num6);
+// displays an error - num6 is an undeclared and 
+// variable names should not be declared with numbers at the start
+
+var ans = 'this is not right';
+// declares the varibale ans twice - this creates an error because you should not do this
+// it is not good practice and can create errors in your code
+
+var let ='do you know what you are doing?';
+// declares a varibale with a keyword 'let' which is used in JS to decalre varibales
+// this can create an error or unwanted outcome
+
+const dad;
+// declares a constant named dad but does not initialize it with a value 
+// you must initialize a constant with a value after it is delcared
+
+const myNum = num1>num2? num3:us;
+// declares a constant named 'myNum' and initializes it with -> 
+// a ternary operator -> the condition asks if num1 is greater than num2 -> if true it returns num3
+// if false it returns the value of the 'us' constant and assigns it to the constant 'myNum'
+
+console.log(--num3);
+// displays the variable num3 and decrements it by one
+
+console.log(num3);
+// displays the variable num3
+
+console.log(num3++);
+// displays the variable num3 and increments it by one
+
+console.log(num2 != num3);
+// displays whether num2 is not equal to num3 -> boolean of 'true'
+
+console.log(num3 !=num2 && num2==num2);
+// displays the boolean 'true' 
+// if num3 is not equal to num2 AND
+// num2 is equal to itself
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+// if 100 is greater than or equal to 200
+// declares and initalizes the constant num2 which is a local variable
+// displays to the terminal the string - 'you are now a junior developer'
+
+
+
+
+
+
+
+
+
+
+
---
+++ Isaac/javaScript/mukulembezewilfirstAssignment.js
@@ -0,0 +1,59 @@
+let you =" are the reason";
+//declare a variable called 'you' and initialize it by assigning to it the string value " are the reason".
+var we = " the next generation of programmers";
+//declare a variable with the name "we" and initialize it by assigning to it the string value " the next generation of programmers".
+const us = ' we are upcoming software problem solvers';
+//declare a variable with the name "us" initializing it by assigning it the string value ' we are upcoming software problem solvers'.
+let num1 = 5;
+//declare a variable with the name num1, initialize it by assigning to it the number value 5.
+let num2 = 10;
+//declare a variable with the name num2, initialize it by assigning to it the number value 10.
+let num3 = num2 += num1;
+//declare a variable with the name num3 and assign it the value of the sum of two variables namely num2 and num1. This implies the value in num3 is 15.
+let ans = num1 % num2;
+//declare a variable with the name ans and assign it the value of the remainder of dividing the value of the variable num1 by the value of the variable num2. This implies the value of ans is 5.
+console.log(ans);
+//print or display the current value in the variable ans which is 5.
+console.log(num3 *= 2);
+//multiply the value in the variable named num3 by 2 and print the answer. This returns 30.
+var num4;
+//declare a variable with the name num4 that is not initialized.
+console.log(num4);
+//print the value of the variable called num4. This returns 'undefined' since variable num4 is not initialized.
+console.log(2num6);
+//this returns an error as it tries to print the value of a variable but the variable name begins with a number and a variable name should not begin with a number.
+var ans = 'this is not right';
+//This will return an error because the variable ans has already been declared.
+var let ='do you know what you are doing?';
+//This declares a variable with the name of a JavaScript key word. This is not advisable.
+const dad;
+//This will return an error because a variable declared with const MUST be initialized.
+const myNum = num1>num2? num3:us;
+//declare a variable with the name myNum and assign it the same value as that of the variable num3 if the value in num1 is greater than the value in num2 and if not then assign it the same value as that of the variable us.
+console.log(--num3);
+//first decrease or decrement the variable num3 then print its value. Returns 29.
+console.log(num3);
+//prints the value in the variable num3 which in this case is 29.
+console.log(num3++);
+//first prints the value in the variable num3 then increments or increases it by 1.
+console.log(num2 != num3);
+//print true or false depending on whether the value in the variable num2 is not equal to that in num3. In this case this is true.
+console.log(num3 !=num2 && num2==num2);
+//print true or false if the value in num3 is not equal to that in num2 and the value in nume2 is equal to the value in num2. In this case this returns true.
+
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+
+//if it is true that 100 is greater or equal to 200, assign the number value 20 to the variable num2 and print the text string 'you are now a junior developer'.
+
+
+
+
+
+
+
+
+
