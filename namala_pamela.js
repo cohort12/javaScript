bfc7334f2b5de29337429c865d5c723425ec6004
Pamela Namala <namala.pamela@gmail.com>
2022-09-14 12:27:21 +0300

    Assignment2

--- namala_pamela.js
+++ namala_pamela.js
@@ -1,23 +1,23 @@
 let you =" are the reason";
-//declared a variable 'you' using the function 'let' and assigned it a string value ' are the reason' using = operator
+//declared a variable 'you' using the keyword 'let' and assigned it a string value ' are the reason' using = operator
 var we = " the next generation of programmer";
-//declared a variable 'we' using the function 'var' and assigned it a string value ' the next generation of programmer' using = operator
+//declared a variable 'we' using the keyword 'var' and assigned it a string value ' the next generation of programmer' using = operator
 const us = ' we are upcoming software problem solvers';
-//declared a constant/unchanging variable 'us' using the function 'const' and assigned it the string value ' we are upcoming software problem solvers' using = operator
+//declared a constant/unchanging variable 'us' using the keyword 'const' and assigned it the string value ' we are upcoming software problem solvers' using = operator
 let num1 = 5;
-//Declared a variable 'num1' using the function 'let' and assigned it an integer value 5 using = operator
+//Declared a variable 'num1' using the keyword 'let' and assigned it an integer value 5 using = operator
 let num2 = 10;
-//Declared a variable 'num2' using the function 'let' and assigned it an integer value 10 using = operator
+//Declared a variable 'num2' using the keyword 'let' and assigned it an integer value 10 using = operator
 let num3 = num2 += num1;
-//Declared a variable 'num3' using the function 'let' and used the = operator to assign it as an increment on from adding num2 to num3
+//Declared a variable 'num3' using the keyword 'let' and used the = operator to assign it as an increment on from adding num2 to num3
 let ans = num1 % num2;
-//Declared a variable 'ans' using the function 'let' and used the = operator to assign it as a result value from dividing num1 by num2
+//Declared a variable 'ans' using the keyword 'let' and used the = operator to assign it as a result value from dividing num1 by num2
 console.log(ans);
 //This prints ans
 console.log(num3 *= 2);
 //This prints the result of multiplying num3 by 2
 var num4;
-//Declaring a a variable 'num4' using the function let and leaving the variable empty
+//Declaring a a variable 'num4' using the keyword 'var' and leaving the variable empty
 console.log(num4);
 //This returns 'Undefinied variable' because 'num4' was not assigned a value
 console.log(2num6);
@@ -25,9 +25,9 @@ console.log(2num6);
 var ans = 'this is not right';
 //This returns an error because a variable can only be declared once
 var let ='do you know what you are doing?';
-//This is unacceptable in Javascript because let is a keyword
+//Declaring a variable as 'let' is not acceptable in Javascript because 'let' is a keyword
 const dad;
-//This will not work because when using 'const' to declare a constant variable, the variable cannot be left empty
+//This will not work because when using keyword 'const' to declare a constant variable, the variable cannot be left empty
 const myNum = num1>num2? num3:us;
 //This declares a constant variable 'myNum' and assigns it a value 'num3' if the value of num1 is greater than the value of num2. If the value num1 is not greater than num2, 'myNum' is assigned the value 'us'.
 console.log(--num3);
