562c4e73044ce4738e96fd03d6ae91fd29456348
Pamela Namala <namala.pamela@gmail.com>
2022-09-14 15:22:14 +0300

    Assignment

---
+++ namala_pamela.js
@@ -0,0 +1,60 @@
+let you =" are the reason";
+//declared a variable 'you' using the keyword 'let' and assigned it a string value ' are the reason' using = operator
+var we = " the next generation of programmer";
+//declared a variable 'we' using the keyword 'var' and assigned it a string value ' the next generation of programmer' using = operator
+const us = ' we are upcoming software problem solvers';
+//declared a constant/unchanging variable 'us' using the keyword 'const' and assigned it the string value ' we are upcoming software problem solvers' using = operator
+let num1 = 5;
+//Declared a variable 'num1' using the keyword 'let' and assigned it an integer value 5 using = operator
+let num2 = 10;
+//Declared a variable 'num2' using the keyword 'let' and assigned it an integer value 10 using = operator
+let num3 = num2 += num1;
+//Declared a variable 'num3' using the keyword 'let' and used the = operator to assign it as an increment on from adding num2 to num3
+let ans = num1 % num2;
+//Declared a variable 'ans' using the keyword 'let' and used the = operator to assign it as a result value from dividing num1 by num2
+console.log(ans);
+//This prints ans
+console.log(num3 *= 2);
+//This prints the result of multiplying num3 by 2
+var num4;
+//Declaring a a variable 'num4' using the keyword 'var' and leaving the variable empty
+console.log(num4);
+//This returns 'Undefinied variable' because 'num4' was not assigned a value
+console.log(2num6);
+//This returns a syntax error because a variable name cannot begin with a number
+var ans = 'this is not right';
+//This returns an error because a variable can only be declared once
+var let ='do you know what you are doing?';
+//Declaring a variable as 'let' is not acceptable in Javascript because 'let' is a keyword
+const dad;
+//This will not work because when using keyword 'const' to declare a constant variable, the variable cannot be left empty
+const myNum = num1>num2? num3:us;
+//This declares a constant variable 'myNum' and assigns it a value 'num3' if the value of num1 is greater than the value of num2. If the value num1 is not greater than num2, 'myNum' is assigned the value 'us'.
+console.log(--num3);
+//This decreases the value of num3 by 1 before printing it
+console.log(num3);
+//This prints the new value of num3
+console.log(num3++);
+//This increases the value of num3 by 1 after printing it
+console.log(num2 != num3);
+//This will return a boolean value 'true' since the value in the variable num2 is not equal to that in num3.
+console.log(num3 !=num2 && num2==num2);
+//This will return the boolean value 'true' since the value in num3 is different from the value in num2 AND the value in num2 is equal to the value in num2.
+
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+//Since the condition of the code '100 being greater than or equal to 200' is not true, the code will return an empty output.
+
+
+
+
+
+
+
+
+
+
+
